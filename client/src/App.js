c39afd0e2a8bb2f57aef0dbb3d542f26604cb97c
Costa <ancst@yandex.ru>
2022-02-04 15:55:41 +0300

    App and Redux init

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
+// import { Provider } from "react-redux";
+// import { store } from "../../redux/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from './components/Home/Home'
+// import Registration from "./components/Registration/Registration";
+// import Login from "./components/Login/Login";
+// import Profile from "./components/Profile/Profile";
+// import CurrentProfile from "./components/CurrentProfile/CurrentProfile";
+// import EventsList from "./components/EventsList/EventsList";
+// import CurrentEvent from "./components/CurrentEvent/CurrentEvent";
+// import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
-    <div>
-       Check App
-    </div>
+    // <Provider store={store}>
+      <BrowserRouter>
+        <h1>
+          Hello APP
+        </h1>
+        <Routes>
+          {/* <Route path='/' element={<Home />} />
+          <Route path='/registration' element={<Registration />} />
+          <Route path='/login' element={<Login />} /> */}
+          {/* <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/:id' element={<CurrentProfile />} />
+          <Route path='/events' element={<EventsList />} />
+          <Route path='/events/:id' element={<CurrentEvent />} /> */}
+          {/* <Route path='*' element={<NotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
+    // </Provider>
   );
 }
 
