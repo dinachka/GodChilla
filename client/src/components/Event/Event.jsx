ee29b51ee24e33bc2b19aed76488274b2897fd27
Olga <anugallaguna@gmail.com>
2022-02-04 16:48:51 +0300

    add eventlist & event components

---
+++ client/src/components/Event/Event.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CurrentEvent({event}) {
+  const users = [{id: 1, name: 'Олех', surname: 'Смирнов', photo: 'https://avatars.mds.yandex.net/get-ott/374297/2a000001616b87458162c9216ccd5144e94d/678x380'}]
+  const user = users.find( el => el.id === event.userID)
+  const clickJoinHandler = () => {}
+  return (
+    <div>
+      { event.photo ? <img src='https://pbs.twimg.com/profile_images/445338647261229056/Gf5tt71x_400x400.jpeg' alt='not found'></img>
+      : <img src={`/pictures/${event.category}.jpeg`} alt='not found'></img> }
+      <div> Заголовок </div>
+      <div> { event.title } </div>
+      <div> Локация </div>
+      <div> { event.location } </div>
+      <div> Дата </div>
+      <div> { event.dateTime } </div>
+      <div> Описание </div>
+      <div> { event.description } </div>
+      <div> Инициатор </div>
+      <Link to={`/profile/${event.userID}`}> { user.name } { user.surname } { user.photo && <img style={{maxWeigth: '2REM', maxHeight: "2REM"}} src={user.photo} alt=""/>}</Link>
+      <button onClick={clickJoinHandler}>Присоединиться</button>
+    </div>
+  );
+}
+
+export default CurrentEvent;
