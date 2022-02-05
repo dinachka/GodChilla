3372299bee42bc578215a9a1a5ea15e7a1ae291a
Ayusha <ayushamikhaylov@gmail.com>
2022-02-05 11:03:27 +0300

    route changes

--- server/routes/deleteEvent.routes.js
+++ server/routes/deleteEvent.routes.js
@@ -1,28 +1,28 @@
-const router = require('express').Router();
-const { Event } = require('../db/models');
+// const router = require('express').Router();
+// const { Event } = require('../db/models');
 
-router.delete('/api/event/:id', async (req, res) => {
-  const eventId = req.params.id;
-  try {
-    const deleted = await Event.destroy({
-      where: {
-        id: eventId,
-      },
-    });
-    if (deleted) {
-      res.status(200).json({
-        message: 'Событие удалилось успешно, создайте новое!',
-      });
-    } else {
-      res.status(400).json({
-        message: 'Событие не удалилось, попробуйте снова!',
-      });
-    }
-  } catch {
-    res.status(400).json({
-      message: 'Что-то пошло не так, попробуйте снова!',
-    });
-  }
-});
+// router.delete('/:id', async (req, res) => {
+//   const eventId = req.params.id;
+//   try {
+//     const deleted = await Event.destroy({
+//       where: {
+//         id: eventId,
+//       },
+//     });
+//     if (deleted) {
+//       res.status(200).json({
+//         message: 'Событие удалилось успешно, создайте новое!',
+//       });
+//     } else {
+//       res.status(400).json({
+//         message: 'Событие не удалилось, попробуйте снова!',
+//       });
+//     }
+//   } catch {
+//     res.status(400).json({
+//       message: 'Что-то пошло не так, попробуйте снова!',
+//     });
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
