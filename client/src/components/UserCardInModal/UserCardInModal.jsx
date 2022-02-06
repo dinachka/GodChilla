8eb019100a86f40071ea8a279cb1bef178a6e18d
Costa <ancst@yandex.ru>
2022-02-06 21:36:43 +0300

    button in getFriendsLogic

--- client/src/components/UserCardInModal/UserCardInModal.jsx
+++ client/src/components/UserCardInModal/UserCardInModal.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import './userCardInModal.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { addFriendshipFetchAC } from '../../redux/actionCreatorsAsync/friendsACAsync';
 
 function UserCardInModal({ user }) {
+
+  const mainUser = useSelector(state => state.userReducer)
+  const dispatch = useDispatch();
+
+  const getFriendship = (event) => {
+    const idForFriends = {
+      reqUserId: mainUser.id,
+      resUserId: user.id
+    }
+    event.preventDefault();
+    dispatch(addFriendshipFetchAC(idForFriends))
+  }
+  console.log(mainUser.user.id);
+
   return (
     <div className='userCardInModal' >
       <div>{ user.name }</div>
       <div>{ user.lastName }</div>
-      <button>Добавить в друзья!</button>
+      <button onClick={getFriendship} >Добавить в друзья!</button>
       <br />
     </div>
   )
