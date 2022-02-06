3b7069e898c1287d9fbdb8e9662594635c9cfc4d
Ayusha <ayushamikhaylov@gmail.com>
2022-02-06 15:42:11 +0300

    addEventAC

--- client/src/redux/actionCreators/eventAC.js
+++ client/src/redux/actionCreators/eventAC.js
@@ -1,7 +1,15 @@
-import { INIT_PUBLIC_EVENTS } from '../actionTypes/eventAT'
+import { INIT_PUBLIC_EVENTS, FETCH_POST_EVENT } from '../actionTypes/eventAT'
+
 export const getPublicEvents = (payload) => {
   return {
     type: INIT_PUBLIC_EVENTS,
+    payload
+  }
+}
+
+export const addEventAC = (payload) => {
+  return {
+    type: FETCH_POST_EVENT,
     payload
   }
 }
