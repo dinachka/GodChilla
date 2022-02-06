612919d25637e4e5b8992ea56ff26da1a2eeadce
Ayusha <ayushamikhaylov@gmail.com>
2022-02-06 12:53:33 +0300

    eventCreator

--- client/src/components/EventCreator/EventCreator.jsx
+++ client/src/components/EventCreator/EventCreator.jsx
@@ -1,31 +1,56 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import './eventCreator.css'
 
 function EventCreator() {
+
+  // const { id } = useParams()
+  const nameInput = useRef()
+  const descriptionInput = useRef()
+  const placeInput = useRef()
+  const dateInput = useRef()
+  const photoInput = useRef()
+  const id = useSelector(state => state.userReducer)
+
+  const navigate = useNavigate()
+
+  const eventHandler = (event) => {
+    event.preventDefault()
+
+    console.log(id, nameInput.current.value, descriptionInput.current.value, placeInput.current.value, dateInput.current.value, photoInput.current.value);
+
+    const newEvent = {
+      userID: id
+    }
+
+    // navigate('/profile')id
+  }
+
   return (
-      <form action="/event" method="post" encType="multipart/form-data">
+      <form type='submit' onSubmit={eventHandler} action="/event" method="post" encType="multipart/form-data">
         <label className='eventCreatorForm'>Название 
-          <input className='eventCreatorForm' type="text" required/>
+          <input ref={nameInput} className='eventCreatorForm' type="text" required/>
         </label>
 
         <br />
         <label className='eventCreatorForm'>Описание 
-          <input className='eventCreatorForm' type="text" required/>
+          <input ref={descriptionInput} className='eventCreatorForm' type="text" required/>
         </label>
 
         <br />
         <label className='eventCreatorForm'>Место проведения 
-          <input className='eventCreatorForm' type="text"/>
+          <input ref={placeInput} className='eventCreatorForm' type="text"/>
         </label>
 
         <br />
         <label className='eventCreatorForm'>Дата проведения 
-          <input className='eventCreatorForm' type="date"/>
+          <input ref={dateInput} className='eventCreatorForm' type="date"/>
         </label>
 
         <br />
         <label className='eventCreatorForm'>Фото
-          <input className='eventCreatorForm' type="file" name="photo" />
+          <input ref={photoInput} className='eventCreatorForm' type="file" name="photo" />
         </label>
         
         <br />
