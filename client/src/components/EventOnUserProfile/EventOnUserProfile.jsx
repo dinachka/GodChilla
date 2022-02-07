0cfb599a4cd767285a490cb73cf1c6ca97d7e4b1
Ayusha <ayushamikhaylov@gmail.com>
2022-02-07 10:52:27 +0300

    edit & delete handles on usrs events

--- client/src/components/EventOnUserProfile/EventOnUserProfile.jsx
+++ client/src/components/EventOnUserProfile/EventOnUserProfile.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 
-function EventOnUserProfile({event}) {
+function EventOnUserProfile({ event }) {
+  const deleteHandle = event => {
+    event.preventDefault();
+    console.log('delete');
+  };
+
+  const editHandle = event => {
+    event.preventDefault();
+    console.log('edit');
+  };
+
   return (
     <div>
-       <div>
-      { event.photo ? <img src='https://pbs.twimg.com/profile_images/445338647261229056/Gf5tt71x_400x400.jpeg' alt='not found'></img>
-      : <img src={`/pictures/${event.categoryID}.jpg`} alt='not found'></img> }
-      <div> Заголовок </div>
-      <div> { event.title } </div>
-      <div> Локация </div>
-      <div> { event.location } </div>
-      <div> Дата </div>
-      <div> { event.dateTime } </div>
-      <div> Описание </div>
-      <div> { event.description } </div>
-    </div>
+      <div>
+        {event.photo ? (
+          <img
+            src="https://pbs.twimg.com/profile_images/445338647261229056/Gf5tt71x_400x400.jpeg"
+            alt="not found"
+          ></img>
+        ) : (
+          <img src={`/pictures/${event.categoryID}.jpg`} alt="not found"></img>
+        )}
+        <div> Заголовок: {event.title} </div>
+        <div> Локация: {event.location} </div>
+        <div> Дата: {event.dateTime} </div>
+        <div> Описание: {event.description} </div>
+        <button onClick={editHandle}>Изменить событие</button>
+        <button onClick={deleteHandle}>Отменить событие</button>
+      </div>
     </div>
   );
 }
