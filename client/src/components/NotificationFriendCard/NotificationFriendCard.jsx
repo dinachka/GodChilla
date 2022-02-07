1cc24f7c448173e4e473a5571b2512142c045b10
dina <dinachernykh1@gmail.com>
2022-02-07 18:09:52 +0300

    update seeds

--- client/src/components/NotificationFriendCard/NotificationFriendCard.jsx
+++ client/src/components/NotificationFriendCard/NotificationFriendCard.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 function NotificationFriendCard({user}) {
+const dispatch = useDispatch()
+
+  const acceptFriendship = () => {
+    dispatch()
+  }
+
+  const rejectFriendship = () => {
+    dispatch()
+  }
   return (
+    <>
     <div>
-      {user.name}
+      {user.name} {user.lastName}
+      <button onClick={acceptFriendship}>Принять</button> <button onClick={rejectFriendship}>Отклонить</button>
     </div>
+    </>
   );
 }
 
