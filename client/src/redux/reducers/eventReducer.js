f02071376b3ce787800a812feb188872aefcebcd
dina <dinachernykh1@gmail.com>
2022-02-07 09:57:48 +0300

    fixing eventslist

--- client/src/redux/reducers/eventReducer.js
+++ client/src/redux/reducers/eventReducer.js
@@ -6,6 +6,7 @@ export const eventReducer = (state = initialState, action) => {
   switch (action.type) {
     case INIT_PUBLIC_EVENTS:
       return { ...state, events: action.payload.events, friendsId: action.payload.friendsId 
+      }
 
     case INIT_USERS_EVENTS: 
       return { ...state, userEvents: action.payload.events }
