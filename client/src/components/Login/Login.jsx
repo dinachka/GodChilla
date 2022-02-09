efadb8c71f655f3e8c1392d728134e52c1bcb021
Olga <anugallaguna@gmail.com>
2022-02-09 12:58:16 +0300

    login error handler and redirect

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useRef, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { loginFetchAC } from '../../redux/actionCreatorsAsync/userACAsync'
 import './login.css'
@@ -8,6 +8,8 @@ function Login(props) {
   const emailRef = useRef();
   const passwordRef = useRef();
   const navigate = useNavigate();
+
+  const { user } = useSelector(state => state.userReducer)
   
   const dispatch = useDispatch();
   const submitLoginHandler = (event) => {
@@ -17,13 +19,21 @@ function Login(props) {
         password: passwordRef.current.value,
       }
       dispatch(loginFetchAC(payload))
-      navigate('/')   
+      // navigate('/')   
   }
+
+  useEffect(() => {
+    if(user?.isUser) {
+      navigate('/profile') 
+    } 
+  }, [user, navigate])
+
   return (
 
       <form onSubmit={submitLoginHandler} className='form' >
         <label>email <input type="email" ref={emailRef} required/></label>
         <label>Пароль <input type="password" autoComplete="off" ref={passwordRef} required/></label>
+        { !user?.isUser && user?.message !== 'Сессия не найдена' &&  <div style={{color:'red', width:'147px', margin:'auto'}}>{user?.message}</div>}
         <button className='btn btnStyle' >Войти</button>
       </form>
   );
