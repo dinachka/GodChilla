141a099d3adff68f9f72e9a6d89446ecedb588a2
Tkeshick <dianatkesh@gmail.com>
2022-02-10 18:14:36 +0300

    styles for friends on profile

--- client/src/components/FriendCard/FriendCard.jsx
+++ client/src/components/FriendCard/FriendCard.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './FriendCard.css'
 
 function FriendCard({ friend }) {
 
   return (
     <Link to={`/profile/user/${friend.id}`} >
-      <div className="uk-card uk-card-default uk-animation-slide-bottom" style={{ display: 'flex', maxHeight: '100px' }}>
-        <div className="uk-card-header">
-          <div className="uk-grid-small uk-flex-middle" style={{ display: 'flex' }}>
-            <div className="uk-width-auto">
-              <img className="uk-border-circle" width="30" height="30" src={friend.photo} alt='friends ava' />
-            </div>
-            <div className="uk-width-expand">
-              <h3 className="uk-card-title uk-margin-remove-bottom">{friend.name}</h3>
-              <p className="uk-text-meta uk-margin-remove-top">{friend.lastName}</p>
-            </div>
-          </div>
+      <div className="uk-card uk-card-default uk-animation-slide-bottom friendsCard_box">
+        <div className="uk-width-auto friendsCard_box__img">
+          <img className="uk-border-circle" width="40" height="40" src={friend.photo} alt='friends ava' />
+        </div>
+        <div className=" friendsCard_box__friendsName">
+          <h3 className="uk-card-title uk-margin-remove-bottom">{friend.name}</h3>
+          <p className="uk-text-meta uk-margin-remove-top">{friend.lastName}</p>
         </div>
       </div>
     </Link>
