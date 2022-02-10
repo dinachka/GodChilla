95d96fe55a3939df772be438e976acda67253e47
Tkeshick <dianatkesh@gmail.com>
2022-02-10 10:41:45 +0300

    image path returns to function

--- client/src/components/EventCreator/EventCreator.jsx
+++ client/src/components/EventCreator/EventCreator.jsx
@@ -62,6 +62,7 @@ function EventCreator({ setSwitcher }) {
 
     const result = await fetch(sendImageToServerURL, options)
     const response = await result.json()
+    console.log(response);
     setEventImg(response)
     // .then(res => res.json())
     // .then(imgPath => setEventImg(imgPath))
