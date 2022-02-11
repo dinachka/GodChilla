b3be218f09729b8533943d6d4c62b1974000a76e
Olga <anugallaguna@gmail.com>
2022-02-11 15:12:55 +0300

    flexbox in filter

--- client/src/components/EventsList/EventsList.jsx
+++ client/src/components/EventsList/EventsList.jsx
@@ -68,13 +68,13 @@ function EventsList(props) {
           </label>
         </form>
         <form >
-          <label>посиделки</label> <input type="checkbox" ref={cozyRef} onChange={filferHandler} defaultChecked="checked" value={1}/>
-          <label>отдых на природе<input type="checkbox" ref={natureRef} onChange={filferHandler} defaultChecked="checked" value={2}/></label>
-          <label>культура, зрелищные мероприятия<input type="checkbox" onChange={filferHandler} defaultChecked="checked" ref={cultureRef} value={3}/></label>
-          <label>прогулка/поездка<input type="checkbox" ref={walkRef} onChange={filferHandler} defaultChecked="checked" value={4}/></label>
-          <label>активный отдых, спорт<input type="checkbox" onChange={filferHandler} defaultChecked="checked" ref={sportRef} value={5}/></label>
-          <label>творчество<input type="checkbox" ref={creativeRef} onChange={filferHandler} defaultChecked="checked" value={6}/></label> 
-          <label>кафе, бар, ресторан<input type="checkbox" ref={cafeRef} onChange={filferHandler} defaultChecked="checked" value={7}/></label>
+          <div style={{display:'flex'}}><label style={{flexBasis: '50%'}}>посиделки</label><input style={{flexBasis: '50%'}} type="checkbox" ref={cozyRef} onChange={filferHandler} defaultChecked="checked" value={1}/></div> 
+          <div style={{display:'flex'}}><label style={{flexBasis: '50%'}}>отдых на природе</label><input style={{flexBasis: '50%'}} type="checkbox" ref={natureRef} onChange={filferHandler} defaultChecked="checked" value={2}/></div>
+          <div style={{display:'flex'}}><label style={{flexBasis: '50%'}}>культура, зрелищные мероприятия</label><input style={{flexBasis: '50%'}} type="checkbox" onChange={filferHandler} defaultChecked="checked" ref={cultureRef} value={3}/></div>
+          <div style={{display:'flex'}}><label style={{flexBasis: '50%'}}>прогулка/поездка</label><input style={{flexBasis: '50%'}} type="checkbox" ref={walkRef} onChange={filferHandler} defaultChecked="checked" value={4}/></div>
+          <div style={{display:'flex'}}><label style={{flexBasis: '50%'}}>активный отдых, спорт</label><input style={{flexBasis: '50%'}} type="checkbox" onChange={filferHandler} defaultChecked="checked" ref={sportRef} value={5}/></div>
+          <div style={{display:'flex'}}><label style={{flexBasis: '50%'}}>творчество</label><input style={{flexBasis: '50%'}} type="checkbox" ref={creativeRef} onChange={filferHandler} defaultChecked="checked" value={6}/></div>
+          <div style={{display:'flex'}}><label style={{flexBasis: '50%'}}>кафе, бар, ресторан</label><input style={{flexBasis: '50%'}} type="checkbox" ref={cafeRef} onChange={filferHandler} defaultChecked="checked" value={7}/></div>
         </form>
           <label className='eventCreatorForm'>Дата проведения 
             <input ref={dateInput} onChange={filferHandler} className='eventCreatorForm' type="date"/>
